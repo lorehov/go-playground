6ea1887ea866fdd4dfaa0d03a83e545d214d34e5
lorekhov <lorekhov@yandex-team.ru>
2015-02-15 19:31:43 +0500

    some oauth2

--- web.go
+++ web.go
@@ -1,20 +1,64 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
+	"fmt"
+	"github.com/GoIncremental/negroni-oauth2"
+	"github.com/goincremental/negroni-sessions"
+	"github.com/goincremental/negroni-sessions/cookiestore"
+	"github.com/gorilla/mux"
+	"net/http"
+	"os"
 )
 
 func main() {
-    http.HandleFunc("/", hello)
-    fmt.Println("listening...")
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-    if err != nil {
-      panic(err)
-    }
-}
+	var (
+		clientID     = "c65e55f08cc310a2804f"
+		clientSecret = "42bf39bb1ed11ece4e26430d4b57fc3e5d4f8631"
+		redirectURL  = "https://quiet-depths-2219.herokuapp.com/"
+	)
 
-func hello(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(res, "hello, world")
+	secureMux := mux.NewRouter()
+
+	// Routes that require a logged in user
+	// can be protected by using a separate route handler
+	// If the user is not authenticated, they will be
+	// redirected to the login path.
+	secureMux.HandleFunc("/restrict", func(w http.ResponseWriter, req *http.Request) {
+		token := oauth2.GetToken(req)
+		fmt.Fprintf(w, "OK: %s", token.Access())
+	})
+
+	secure := negroni.New()
+	secure.Use(oauth2.LoginRequired())
+	secure.UseHandler(secureMux)
+
+	n := negroni.New()
+	n.Use(sessions.Sessions("my_session", cookiestore.New([]byte("secret123"))))
+	n.Use(oauth2.Github(&oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		RedirectURL:  redirectURL,
+		Scopes:       []string{"user"},
+	}))
+
+	router := mux.NewRouter()
+
+	//routes added to mux do not require authentication
+	router.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		token := oauth2.GetToken(req)
+		if token == nil || !token.Valid() {
+			fmt.Fprintf(w, "not logged in, or the access token is expired")
+			return
+		}
+		fmt.Fprintf(w, "logged in")
+		return
+	})
+
+	//There is probably a nicer way to handle this than repeat the restricted routes again
+	//of course, you could use something like gorilla/mux and define prefix / regex etc.
+	router.Handle("/restrict", secure)
+
+	n.UseHandler(router)
+
+	n.Run(":3000")
 }
