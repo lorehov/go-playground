a6a6b6c422e43eb66fca39d8f9bf60bc47c2c025
lorekhov <lorekhov@yandex-team.ru>
2015-02-15 20:43:21 +0500

    port

--- web.go
+++ web.go
@@ -14,7 +14,7 @@ func main() {
 	var (
 		clientID     = "c65e55f08cc310a2804f"
 		clientSecret = "42bf39bb1ed11ece4e26430d4b57fc3e5d4f8631"
-		redirectURL  = "https://quiet-depths-2219.herokuapp.com/"
+		redirectURL  = "http://app-3a214f62-5196-4ca9-aa63-9f2f90298127.cleverapps.io/"
 	)
 
 	secureMux := mux.NewRouter()
@@ -46,6 +46,7 @@ func main() {
 	//routes added to mux do not require authentication
 	router.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		token := oauth2.GetToken(req)
+		fmt.Fprintf(w, fmt.Sprintf("%s", token))
 		if token == nil || !token.Valid() {
 			fmt.Fprintf(w, "not logged in, or the access token is expired")
 			return
